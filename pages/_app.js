8c41ae3727151fc3369c08fadcb69f85174f8103
cdgmx <iamcdgmx@gmail.com>
2022-01-05 23:38:33 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import Layout from "../components/Layout"
+import Footer from "../components/Footer"
+import Nav from "../components/Nav"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+ return (
+  <Layout>
+  <Nav/>
+  <div className="container">
+      <Component {...pageProps} />
+  </div>
+  <Footer/>
+</Layout>
+  )
 }
 
 export default MyApp
